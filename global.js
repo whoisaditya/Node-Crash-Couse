8da5b8604c0171ba1f3fe76bd62b9757d33cd248
whoisaditya <mitraaditya1357@gmail.com>
2021-05-20 16:17:15 +0530

    Till Session 1.5

---
+++ global.js
@@ -0,0 +1,17 @@
+// Global Object
+
+//console.log(global); //1
+
+/* // 2
+setTimeout(() => {
+    console.log('In the TimeOut');
+    clearInterval(int);
+}, 3000); 
+
+const int = setInterval(() => {
+    console.log('In the Interval');
+}, 1000);
+*/
+
+console.log(__dirname);
+console.log(__filename);
