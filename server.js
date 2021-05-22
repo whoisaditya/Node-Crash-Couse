cbd87b6cbcd9edacaedbad47ad93e398c516b352
whoisaditya <mitraaditya1357@gmail.com>
2021-05-23 03:03:24 +0530

    Lesson 4 Requests and Responses

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('Request Made');
+    console.log(req.url, req.method);
+
+
+    // set header content type 
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './views';
+    switch(req.url)
+    {   
+        case '/' :{
+                path += '/index.html';
+                res.statusCode = 200;
+                break;
+            }
+        case '/about':{
+                path += '/about.html';
+                res.statusCode = 200;
+                break;
+            }
+        case '/about-me':{
+                res.statusCode = 301;
+                res.setHeader('Location', '/about');
+                break;
+            }
+        default :
+        {
+            path +='/404.html';
+            res.statusCode = 404;
+            break;
+        }
+    }
+
+    //send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data); //In case you're just using one thing
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
